cc347a0075ca853353ff988e020a6eda602fd7a6
Mounia Belkheir <mouniabelkheir@outlook.com>
2024-12-10 23:36:42 +0100

    add span tag

--- src/MainSection/Aside/Aside.js
+++ src/MainSection/Aside/Aside.js
@@ -4,24 +4,20 @@ export default function Aside() {
 	return (
 		<aside>
 			<Button tag=" جديدة">
-				<>&#x1F4AB;</>
-				<>&#x1F4AB;</>
-				<>&#x1F4AB;</>
+				<span>&#x1F4AB; &#x1F4AB; &#x1F4AB;</span>
 			</Button>
 			<Button tag="الأكثر قراءة">
 				<br />
-				<>&#x2B50;</>
-				<>&#x2B50;</>
-				<>&#x2B50;</>
+				<span>&#x2B50; &#x2B50; &#x2B50;</span>
 				<img
-					style={{ borderRadius: "5px", marginTop: '5px' }}
+					style={{ borderRadius: "5px", marginTop: "5px" }}
 					src="https://placehold.co/100x50/pink/FFFFFF.png"
 					alt="flower"
 				/>
 			</Button>
 			<Button tag="مقالات مميزة">
 				<img
-					style={{ borderRadius: "5px", marginTop: '5px' }}
+					style={{ borderRadius: "5px", marginTop: "5px" }}
 					src="https://placehold.co/100x100/orange/FFFFFF.png"
 					alt="flower"
 				/>
